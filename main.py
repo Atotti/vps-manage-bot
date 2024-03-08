b715b4b896923189e8ded954aebc8dbf551ce785
Ayuto <Ayuto1215@outlook.com>
2024-03-09 01:44:00 +0900

    update

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import discord
 from discord.ext import commands
-from discord import app_commands
 import subprocess
 import os
 from dotenv import load_dotenv
@@ -11,10 +10,12 @@ load_dotenv()
 # 環境変数からトークンを読み込む
 DISCORD_BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
 
+
 # Discord Botのセットアップ
 intents = discord.Intents.default()
-client = discord.Client(intents=intents)
-tree = app_commands.CommandTree(client)
+intents.messages = True
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 # サービスを制御する関数
 def control_service(action, service_name):
@@ -25,7 +26,7 @@ def check_service_status(service_name):
     return result.stdout.strip()  # 'active', 'inactive', 'failed'などの状態を返す
 
 # 生活鯖を起動するコマンド
-@tree.command(name="start_minecraft", description="Minecraftサーバーを起動します。")
+@bot.command()
 async def start_minecraft(ctx):
     # Palworldサーバーを停止
     control_service('stop', 'palworld-dedicated.service')
@@ -36,7 +37,7 @@ async def start_minecraft(ctx):
     await ctx.send('Minecraftサーバーを起動しました。')
 
 # 人狼サーバーを起動するコマンド
-@tree.command(name="start_wolf", description="人狼Minecraftサーバーを起動します。")
+@bot.command()
 async def start_wolf(ctx):
     # Palworldサーバーを停止
     control_service('stop', 'palworld-dedicated.service')
@@ -44,10 +45,10 @@ async def start_wolf(ctx):
     control_service('stop', 'minecraft.service')
     # 人狼サーバーを起動
     control_service('start', 'minecraft-werewolf.service')
-    await ctx.send('人狼Minecraftサーバーを起動しました。')
+    await ctx.send('Minecraft人狼サーバーを起動しました。')
 
 # Palworldサーバーを起動するコマンド
-@tree.command(name="start_palworld", description="Palworldサーバーを起動します。")
+@bot.command()
 async def start_palworld(ctx):
     # 生活鯖を停止
     control_service('stop', 'minecraft.service')
@@ -58,14 +59,14 @@ async def start_palworld(ctx):
     await ctx.send('Palworldサーバーを起動しました。')
 
 # サービスの状態を確認するコマンド
-@tree.command(name="check_services", description="サービスの状態を確認します。")
+@bot.command()
 async def check_services(ctx):
     minecraft_status = check_service_status('minecraft.service')
     palworld_status = check_service_status('palworld-dedicated.service')
-    wolf_status = check_service_status('minecraft-werewolf.service')
+    wordwolf_status = check_service_status('minecraft-werewolf.service')
     
-    message = f"Minecraftサーバーの状態: {minecraft_status}\nPalworldサーバーの状態: {palworld_status}\n人狼サーバーの状態: {wolf_status}"
+    message = f"Minecraftサーバーの状態: {minecraft_status}\nPalworldサーバーの状態: {palworld_status}\n 人狼サーバーの状態: {wordwolf_status}"
     await ctx.send(message)
 
 # Botを起動
-client.run(DISCORD_BOT_TOKEN)
+bot.run(DISCORD_BOT_TOKEN)
